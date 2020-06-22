9d16bffb6c785e055d7f08a205ffe45b472e305b
Steve Chamales <schamales@trustar.co>
2020-06-22 11:33:46 -0700

    adjusted to account for the fact that the TruStar SDK does make EnclavePermission members bools instead of leaving them as strings.

--- src/trustar_guardduty_lambda_handler/helpers/ts/enclave_permissions_checker.py
+++ src/trustar_guardduty_lambda_handler/helpers/ts/enclave_permissions_checker.py
@@ -4,6 +4,7 @@
 varios enclaves. """
 
 from logging import getLogger
+
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import *
@@ -16,9 +17,6 @@ logger = getLogger(__name__)                                    # type: Logger
 class EnclavePermissionsChecker:
     """ Verifies that the TruStar client's API creds have the specified
     level of access to the specified enclave. """
-
-    true = 'true'
-    false = 'false'
 
     def __init__(self, ts):                   # type: (TruStar) -> None
         enclaves = ts.get_user_enclaves()     # type: List[EnclavePermissions]
@@ -36,12 +34,12 @@ class EnclavePermissionsChecker:
 
     def can_read(self, enclave_id):                      # type: (str) -> bool
         """ Checks to see if creds have read access to the enclave. """
-        return self.get_perms(enclave_id).read == self.__class__.true
+        return self.get_perms(enclave_id).read
 
     def can_create(self, enclave_id):                    # type: (str) -> bool
         """ Checks to see if creds have create access to the enclave. """
-        return self.get_perms(enclave_id).create == self.__class__.true
+        return self.get_perms(enclave_id).create
 
     def can_update(self, enclave_id):                    # type: (str) -> bool
         """ Checks to see if the creds have update access to the enclave. """
-        return self.get_perms(enclave_id).update == self.__class__.true
+        return self.get_perms(enclave_id).updatee
