a52322de5634c75ed9877d745ca7258d4d6ed111
Steve Chamales <schamales@trustar.co>
2020-06-30 13:20:05 -0700

    simplified the lambda function file a little bit, removed old documentation.

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,24 +1,7 @@
 # encoding = utf-8
 
 """ A script that receives a Guard Duty "Finding" and submits it to an
-enclave as a Report.
-
-- Intended to be used with AWS Lambda.
-
-To deploy:
-
-  pip install --target=. trustar
-  zip -r ../GD-Station-Lambda.zip ./*
-
-  .. then upload the resulting zip file to the Lambda function.
-  You'll also need to set the following environment variables in the function:
-  - TRUSTAR_URL
-  - API_KEY
-  - API_SECRET
-  - ENCLAVE_ID
-
-The associated API credential will need to have write access to the enclave
-defined in the `ENCLAVE_ID` variable. """
+enclave as a Report. """
 
 from trustar_guardduty_lambda_handler import TruStarGuardDutyLambdaHandler
 
@@ -29,5 +12,5 @@ if TYPE_CHECKING:
 
 def lambda_handler(event, context):               # type: (Dict, Dict) -> Dict
     """ Sends Finding to Station. """
-    r = TruStarGuardDutyLambdaHandler.handle(event, context)    # type: Report
-    return r.to_dict()
+    handler = TruStarGuardDutyLambdaHandler()
+    return handler.handle(event, context)                       # type: Report
