09307076c6beb33b964fd2e440a7f27f7cd60eaf
Steve Chamales <schamales@trustar.co>
2020-07-08 21:04:57 -0700

    added trustar SDK to setup.py.

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(name='trustar-guardduty',
       version='0.1.0',
       packages=find_packages(exclude=["tests"]),
-      setup_requires=["setuptools-pipfile"],
+      setup_requires=["setuptools-pipfile",
+                      "trustar==0.3.29"],
       use_pipfile=True)
