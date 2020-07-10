d413d71a63aa88224ba01cf81e94aa67e7a05f0f
Steve Chamales <schamales@trustar.co>
2020-07-09 18:04:28 -0700

    added logic to allow user to specify whether or not to check/compare the upserted report to the report that was actually saved in Station.

--- src/trustar_guardduty_lambda_handler/ts_gd_lambda_handler.py
+++ src/trustar_guardduty_lambda_handler/ts_gd_lambda_handler.py
@@ -2,12 +2,17 @@
 
 """ TruSTAR's Guard Duty Finding Lambda Handler. """
 
+import time
 import os
 from logging import getLogger
+from trustar import IdType
+import datetime
 from .helpers.gd.report_builder import GuardDutyReportBuilder
 from .helpers.ts.enclave_permissions_checker import EnclavePermissionsChecker
 from .helpers.ts.report_upserter import ReportUpserter
 from .helpers.ts.client_builder import ClientBuilder
+from .helpers.ts.report_comparer import ReportComparer
+from .helpers.ts.report_details_fetcher import ReportDetailsFetcher
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -22,6 +27,7 @@ class TruStarGuardDutyLambdaHandler:
     """ The Lambda function handler for Guard Duty events. """
 
     CLIENT_METATAG = "AWS_GUARD_DUTY"
+    VARS_TO_SKIP = ["created", "updated", "id"]
 
     def __init__(self):
         logger.info("Initializing lambda handler.")
@@ -37,11 +43,45 @@ class TruStarGuardDutyLambdaHandler:
 
         self.builder = GuardDutyReportBuilder(destination_enclave)
         self.upserter = ReportUpserter(ts, destination_enclave)
+        self.details_fetcher = ReportDetailsFetcher(ts)
+        self.ts = ts
 
     def handle(self, event):                            # type: (Dict) -> Dict
         """ Processes a Guard-duty event. """
         logger.info("starting lambda handler.")
         report = self.builder.build_for(event)                  # type: Report
-        upserted_report = self.upserter.upsert(report)          # type: Report
-        logger.info("lambda handler complete.")
-        return upserted_report.to_dict()
+        upserted = self.upserter.upsert(report)                 # type: Report
+        if not self.check_saved_report:
+            logger.info("lambda handler complete. returning upserted.")
+            return upserted.to_dict()
+
+        saved = self.details_fetcher.fetch_for(
+            upserted.external_id)                       # type: Report or None
+        if not saved:
+            logger.error("Failed to fetch saved report from Station. "
+                         "Ending lambda, returning the upserted report.")
+            return upserted.to_dict()
+
+        _ = ReportComparer.compare(upserted, saved, self.VARS_TO_SKIP)
+        logger.info("lambda handler complete. returning report from "
+                    "enclave.")
+        return saved.to_dict()
+
+    @property
+    def check_saved_report(self):                           # type: () -> bool
+        """ Determines whether the user specified to check the report saved
+        in Station against the report it upserted. """
+        true_msg = "user wants saved report checked."
+        check = False
+        env_var = os.environ.get("RETURN_SAVED_REPORT")
+        if isinstance(env_var, str):
+            if env_var == "True":
+                logger.info(true_msg)
+                check = True
+        elif isinstance(env_var, bool):
+            if env_var:
+                logger.info(true_msg)
+                check = True
+        else:
+            logger.info("user does not want saved report checked.")
+        return check
