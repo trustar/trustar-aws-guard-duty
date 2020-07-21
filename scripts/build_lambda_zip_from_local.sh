9a138b4d841ce22d20582e8d932498ec2cbfcc43
Steve Chamales <schamales@trustar.co>
2020-07-21 07:22:16 -0700

    updated build-script to use pipenv to create the requirements.txt file.

--- scripts/build_lambda_zip_from_local.sh
+++ scripts/build_lambda_zip_from_local.sh
@@ -2,23 +2,32 @@
 # Run this from your local laptop.
 # Pre-reqs:
 #  -create a new GitHub SSH key and install it in your user account on the EC2 instance.
-#  -setup Python 3.7.7 venv on the EC2 instance using the script in "first_time_run" dir.
+#    (that host will need to be able to clone this git repo)
+#  -setup Python 3.7.7 venv on the EC2 instance using the "amzn_linux_pyen_install.sh"
+#    script in "first_time_run" dir.
 #  -in SSH config file on your laptop, make an entry named "gdbuilder" that will
-#   SSH you into the EC2 instance to a user account that has "sudo".  (usu, ec2-user)
-# It will:
+#   SSH you into the EC2 instance to a user account that has "sudo".  (usually 'ec2-user')
+#
+# This script will...:
 #  -SSH into the EC2 instance.
+#  -create a python 3.7.7 virtualenv.
 #  -clone the repo.
-#  -pull & fetch all.
-#  -jump to the EN-422/update_dependencies branch.
-#  -build the .zip
-#  -terminate the SSH.
-#  -scp the .zip from EC2 to local.
+#  -pull & fetch all branches
+#  -jump to the branch specified in the "BRANCH_TO_BUILD" env var on line 28 of this file.
+#  -build the .zip to be uploaded to Lambda.
+#  -terminate the SSH session.
+#  -scp the .zip from EC2 to local laptop (trustar-aws-guard-duty/build/function.zip)
+#  -SSH into the EC2 instance again.
+#  -delete the temp directory that contains the repo cloneed from Git.
+#  -delete the python 3.7.7 virtualenv.
+
+
 
 rm -rf ../build/  # in case it's already there.
 ssh gdbuilder << EOF
-
+BRANCH_TO_BUILD=master
 pyenv install 3.7.7 -s
-pyenv virtualenv-delete gdbuild-3.7.7
+pyenv virtualenv-delete -f gdbuild-3.7.7
 pyenv virtualenv 3.7.7 gdbuild-3.7.7
 pyenv virtualenvs
 pyenv activate gdbuild-3.7.7
@@ -26,7 +35,7 @@ pyenv version
 cat .python-version
 pyenv rehash
 python --version
-pip install --upgrade pip
+pip install --upgrade pip setuptools
 pip list --format=columns
 
 rm -rf temp_build
@@ -36,10 +45,11 @@ git clone git@github.com:trustar/trustar-aws-guard-duty.git
 cd trustar-aws-guard-duty
 git fetch --all
 git pull --all
-git checkout EN-422/update_dependencies
+git checkout $BRANCH_TO_BUILD
 mkdir ./build
 cp -R ./src/* ./build/
-pip install -r ./requirements.txt --target ./build/
+pipenv lock -r > requirements.txt
+pipenv run pip install -r ./requirements.txt --target ./build/
 cd build
 zip -r9 ../function.zip .
 EOF
