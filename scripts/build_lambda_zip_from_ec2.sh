599218cd96d52836f66df04bd8037242828d79ee
Steve Chamales <schamales@trustar.co>
2020-07-21 07:19:51 -0700

    deleting this build script because it's not needed anymore.

--- scripts/build_lambda_zip_from_ec2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run this after running "amzn_linux_pyenv_install.sh".
-# Run this from AmazonLinux2 EC-2 instance while SSHed in as a user with "sudo" priivs.
-# Run this with CWD being the directory this script resides in.
-mkdir ../../build
-cp -R ../src/* ../../build/
-pip install -r ../requirements.txt --target ../../build
-zip -r9 ../../function.zip ../../build/.
